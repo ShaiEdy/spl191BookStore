854dcba4dfe296b05682417243a48233d40605d5
Shai <shaiadi1@gmail.com>
2018-12-02 20:32:44 +0200

    Test

--- src/main/java/bgu/spl/mics/application/passiveObjects/MoneyRegister.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/MoneyRegister.java
@@ -28,36 +28,36 @@ public class MoneyRegister {
 	}
 
 	/**
-     * Saves an order receipt in the money register.
-     * <p>   
-     * @param r		The receipt to save in the money register.
-     */
+	 * Saves an order receipt in the money register.
+	 * <p>
+	 * @param r     The receipt to save in the money register.
+	 */
 	public void file (OrderReceipt r) {
 		orderReceipts.add(r);
 		totalEarning= totalEarning+ r.getPrice();
 	}
-	
+
 	/**
-     * Retrieves the current total earnings of the store.  
-     */
+	 * Retrieves the current total earnings of the store.
+	 */
 	public int getTotalEarnings() {
 		return totalEarning;
 	}
-	
+
 	/**
-     * Charges the credit card of the customer a certain amount of money.
-     * <p>
-     * @param amount 	amount to charge
-     */
+	 * Charges the credit card of the customer a certain amount of money.
+	 * <p>
+	 * @param amount    amount to charge
+	 */
 	public void chargeCreditCard(Customer c, int amount) {
 		// TODO Implement this
 	}
-	
+
 	/**
-     * Prints to a file named @filename a serialized object List<OrderReceipt> which holds all the order receipts 
-     * currently in the MoneyRegister
-     * This method is called by the main method in order to generate the output.. 
-     */
+	 * Prints to a file named @filename a serialized object List<OrderReceipt> which holds all the order receipts
+	 * currently in the MoneyRegister
+	 * This method is called by the main method in order to generate the output..
+	 */
 	public void printOrderReceipts(String filename) {
 		//TODO: Implement this
 	}
