a6e0419b56477df11f9a57f13a00699636d7fc19
Shai <shaiadi1@gmail.com>
2018-12-03 13:05:54 +0200

    13:05

--- src/main/java/bgu/spl/mics/application/passiveObjects/OrderReceipt.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/OrderReceipt.java
@@ -8,21 +8,29 @@ package bgu.spl.mics.application.passiveObjects;
  * You may add fields and methods to this class as you see fit (including public methods).
  */
 public class OrderReceipt {
-	
+
+	private int orderId;
+	private String sellingServiceName;
+	private int customerId;
+	private String bookTitle;
+	private int price;
+	private int issuedTick;
+	private int orderTick;
+	private int processTick;
+
+
 	/**
      * Retrieves the orderId of this receipt.
      */
 	public int getOrderId() {
-		// TODO Implement this
-		return 0;
+		return orderId;
 	}
-	
+
 	/**
      * Retrieves the name of the selling service which handled the order.
      */
 	public String getSeller() {
-		// TODO Implement this
-		return null;
+		return sellingServiceName;
 	}
 	
 	/**
@@ -31,40 +39,35 @@ public class OrderReceipt {
      * @return the ID of the customer
      */
 	public int getCustomerId() {
-		// TODO Implement this
-		return 0;
+		return customerId;
 	}
 	
 	/**
      * Retrieves the name of the book which was bought.
      */
 	public String getBookTitle() {
-		// TODO Implement this
-		return null;
+		return bookTitle;
 	}
 	
 	/**
      * Retrieves the price the customer paid for the book.
      */
 	public int getPrice() {
-		// TODO Implement this
-		return 0;
+		return price;
 	}
 	
 	/**
      * Retrieves the tick in which this receipt was issued.
      */
 	public int getIssuedTick() {
-		// TODO Implement this
-		return 0;
+		return issuedTick;
 	}
 	
 	/**
      * Retrieves the tick in which the customer sent the purchase request.
      */
 	public int getOrderTick() {
-		// TODO Implement this
-		return 0;
+		return orderTick;
 	}
 	
 	/**
@@ -72,7 +75,6 @@ public class OrderReceipt {
      * processing the order.
      */
 	public int getProcessTick() {
-		// TODO Implement this
-		return 0;
+		return processTick;
 	}
 }
