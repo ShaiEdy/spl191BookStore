559b63dc0e026a730d17ca24680b81132e6f5cf2
עדי <agotliber@gmail..com>
2018-12-04 17:19:54 +0200

    17:19

--- src/main/java/bgu/spl/mics/application/services/LogisticsService.java
+++ src/main/java/bgu/spl/mics/application/services/LogisticsService.java
@@ -32,6 +32,6 @@ public class LogisticsService extends MicroService {
 			Future<DeliveryVehicle> deliveryVehicleFuture= sendEvent(acquireVehicleEvent);
 			DeliveryVehicle deliveryVehicle= deliveryVehicleFuture.get();
 			deliveryVehicle.deliver(c.getAddress(),c.getDistance());
-		});
+		}); //
 	}
 }
